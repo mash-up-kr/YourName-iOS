011ca8b774275464ec65a4cd2a9820aa320acc96
이동영(모바일) <o.o.wl.soft@gmail.com>
2021-09-14 01:36:23 +0900

    [Feature]  FLEX, ViewController Template 구현 (#6)
    * :sparkles: FLEX, ViewController Template 구현
    
    * :recycle: 메서드 네이밍 수정, 레이아웃 draw cycle에 맞게 변경
    
    Co-authored-by: 붱이 <dongyoung.lee@naverz-corp.com>

---
+++ YourName/YourName/Sources/Common/Extensions/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  YourName
+//
+//  Created by Booung on 2021/09/10.
+//
+
+import FLEX
+import RxSwift
+import Then
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAttribute()
+        #if DEBUG
+        setupFLEX()
+        #endif
+    }
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        setupLayout()
+    }
+    
+    /// set up attribute(property) of subviews
+    ///
+    ///     textLabel.text = "Hello, world"
+    ///     textLabel.textColor = .systemBlue
+    ///     view.addSubview(textLabel)
+    func setupAttribute() {
+        fatalError("attribute() has not been implemented")
+    }
+    
+    /// set up layout of subviews
+    ///
+    ///       textLabel.translatesAutoresizingMaskIntoConstraints = false
+    ///       NSLayoutConstraint.activate([
+    ///           textLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+    ///           textLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+    ///       ])
+    func setupLayout() {
+        fatalError("layout() has not been implemented")
+    }
+}
+// MARK: - FLEX Tool
+#if DEBUG
+extension ViewController {
+    private func setupFLEX() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(activateFLEX)).then {
+            $0.numberOfTouchesRequired = 4
+        }
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func activateFLEX() {
+        FLEXManager.shared.showExplorer()
+    }
+}
+#endif
