3194bf8dcc1127c97fb6b3a5cd82263a615effe2
SeoYoung <bgd216@naver.com>
2021-09-16 03:37:21 +0900

    ⚡️ deinit 체크 코드 추가

--- YourName/YourName/Sources/Common/Extensions/ViewController.swift
+++ YourName/YourName/Sources/Common/Extensions/ViewController.swift
@@ -22,6 +22,10 @@ class ViewController: UIViewController {
         #endif
     }
     
+    deinit {
+        print("\(String(describing: self)) deinit")
+    }
+    
     
     /// set up attribute(property) of subviews
     ///
