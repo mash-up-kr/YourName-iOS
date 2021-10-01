84bff7f29248ea12132c9dcc4bf46464e7b2d92e
dongyoung-lee <dongyoung.lee@naverz-corp.com>
2021-10-01 23:14:02 +0900

    네비게이션, 타이핑 테스트 코드 작성

--- YourName/YourName/Sources/UI Layer/Screens/CardCreation/CardCreationViewController.swift
+++ YourName/YourName/Sources/UI Layer/Screens/CardCreation/CardCreationViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Booung on 2021/09/30.
 //
 
+import RxOptional
+import RxSwift
 import UIKit
 
 final class CardCreationViewController: ViewController, Storyboarded {
@@ -24,9 +26,72 @@ final class CardCreationViewController: ViewController, Storyboarded {
         render(viewModel)
     }
     
-    private func dispatch(to viewModel: CardCreationViewModel) {}
+    private func dispatch(to viewModel: CardCreationViewModel) {
+        nameField?.rx.text
+            .filterNil()
+            .subscribe(onNext: { [weak self] in self?.viewModel?.typeName($0) })
+            .disposed(by: disposeBag)
+        
+        roleField?.rx.text
+            .filterNil()
+            .subscribe(onNext: { [weak self] in self?.viewModel?.typeRole($0) })
+            .disposed(by: disposeBag)
+        
+        personalityTitleField?.rx.text
+            .filterNil()
+            .subscribe(onNext: { [weak self] in self?.viewModel?.typePersonalityTitle($0) })
+            .disposed(by: disposeBag)
+        
+        personalityKeywordField?.rx.text
+            .filterNil()
+            .subscribe(onNext: { [weak self] in self?.viewModel?.typePersonalityKeyword($0) })
+            .disposed(by: disposeBag)
+        
+        aboutMeTextView?.rx.text
+            .filterNil()
+            .subscribe(onNext: { [weak self] in self?.viewModel?.typeAboutMe($0) })
+            .disposed(by: disposeBag)
+        
+        completeButton?.rx.tap
+            .subscribe(onNext: { [weak self] in self?.viewModel?.tapCompletion() })
+            .disposed(by: disposeBag)
+    }
     
-    private func render(_ viewModel: CardCreationViewModel) {}
+    private func render(_ viewModel: CardCreationViewModel) {
+        if let nameField = self.nameField {
+            viewModel.name.distinctUntilChanged()
+                .bind(to: nameField.rx.text)
+                .disposed(by: disposeBag)
+        }
+        if let roleField = self.roleField {
+            viewModel.role.distinctUntilChanged()
+                .bind(to: roleField.rx.text)
+                .disposed(by: disposeBag)
+        }
+        if let personalityTitleField = self.personalityTitleField {
+            viewModel.personalityTitle.distinctUntilChanged()
+                .bind(to: personalityTitleField.rx.text)
+                .disposed(by: disposeBag)
+        }
+        if let personalityKeywordField = self.personalityKeywordField {
+            viewModel.personalityKeyword.distinctUntilChanged()
+                .bind(to: personalityKeywordField.rx.text)
+                .disposed(by: disposeBag)
+        }
+        if let aboutMeTextView = self.aboutMeTextView {
+            viewModel.aboutMe.distinctUntilChanged()
+                .bind(to: aboutMeTextView.rx.text)
+                .disposed(by: disposeBag)
+        }
+        viewModel.navigation
+            .subscribe(onNext: { [weak self] navigation in
+                let viewController = UIViewController()
+                self?.navigate(viewController, action: navigation.action)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private let disposeBag = DisposeBag()
     
     @IBOutlet private weak var profileClearButton: UIButton?
     @IBOutlet private weak var profilePlaceholderView: UIView?
--- YourName/YourName/Sources/UI Layer/Screens/CardCreation/CardCreationViewModel.swift
+++ YourName/YourName/Sources/UI Layer/Screens/CardCreation/CardCreationViewModel.swift
@@ -6,7 +6,68 @@
 //
 
 import Foundation
+import RxRelay
+
+enum CardCreationDestination: Equatable {
+    case profileBackgroundSetting
+    case createCharacter
+    case mySkillSetting
+    case myTMISetting
+}
+
+typealias CardCreationNavigation = Navigation<CardCreationDestination>
 
 final class CardCreationViewModel {
     
+    // State
+    let shouldShowImageSelectOption = PublishRelay<Void>()
+    let shouldHideClear = BehaviorRelay<Bool>(value: true)
+    let shouldHideProfilePlaceholder = BehaviorRelay<Bool>(value: false)
+    let name = BehaviorRelay<String>(value: .empty)
+    let role = BehaviorRelay<String>(value: .empty)
+    let personalityTitle = BehaviorRelay<String>(value: .empty)
+    let personalityKeyword = BehaviorRelay<String>(value: .empty)
+    let aboutMe = BehaviorRelay<String>(value: .empty)
+    let navigation = PublishRelay<CardCreationNavigation>()
+    
+    // Event
+    func tapProfilePlaceHolder() {
+        shouldShowImageSelectOption.accept(Void())
+    }
+    
+    func tapProfileBackgroundSetting() {
+        navigation.accept(.present(.profileBackgroundSetting))
+    }
+    
+    func typeName(_ text: String) {
+        name.accept(text)
+    }
+    
+    func typeRole(_ text: String) {
+        role.accept(text)
+    }
+   
+    func tapMySkillSetting() {
+        navigation.accept(.present(.mySkillSetting))
+    }
+    
+    func typePersonalityTitle(_ text: String) {
+        personalityTitle.accept(text)
+    }
+    
+    func typePersonalityKeyword(_ text: String) {
+        personalityKeyword.accept(text)
+    }
+    
+    func tapMyTMISetting() {
+        navigation.accept(.present(.myTMISetting))
+    }
+    
+    func typeAboutMe(_ text: String) {
+        aboutMe.accept(text)
+    }
+    
+    func tapCompletion() {
+        
+    }
 }
