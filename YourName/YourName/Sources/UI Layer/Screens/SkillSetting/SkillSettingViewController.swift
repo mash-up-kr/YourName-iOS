e451371b7a7410c03651be04fb3fdb7304fbcb1a
dongyoung-lee <dongyoung.lee@naverz-corp.com>
2021-10-02 06:19:08 +0900

    Setting TMI / Skill ViewController 정의

---
+++ YourName/YourName/Sources/UI Layer/Screens/SkillSetting/SkillSettingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SkillSettingViewController.swift
+//  YourName
+//
+//  Created by Booung on 2021/10/02.
+//
+
+import Foundation
---
+++ YourName/YourName/Sources/UI Layer/Screens/TMISetting/TMISettingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TMISettingViewConrroller.swift
+//  YourName
+//
+//  Created by Booung on 2021/10/02.
+//
+
+import Foundation
