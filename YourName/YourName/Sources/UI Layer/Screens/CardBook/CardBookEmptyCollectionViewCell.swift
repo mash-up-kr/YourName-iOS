b1f6e440e075094d3d354ad0cd23c91b0c3cc7a9
SeoYoung <bgd216@naver.com>
2021-10-03 03:23:33 +0900

    💄 CardBook 디렉토리 및 파일 생성

---
+++ YourName/YourName/Sources/UI Layer/Screens/CardBook/CardBookEmptyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CardBookEmptyCollectionViewCell.swift
+//  YourName
+//
+//  Created by seori on 2021/10/03.
+//
+
+import UIKit
+
+final class CardBookEmptyCollectionViewCell: UICollectionViewCell {
+    
+}
