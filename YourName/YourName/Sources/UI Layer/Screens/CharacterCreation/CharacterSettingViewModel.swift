489d6c16f1924a50dd23bf2d7b12aa2d9aa53386
dongyoung-lee <dongyoung.lee@naverz-corp.com>
2021-10-04 18:45:46 +0900

    :alien: 캐릭터 설정 화면 가구현

--- YourName/YourName/Sources/UI Layer/Screens/CharacterCreation/CharacterSettingViewModel.swift
+++ YourName/YourName/Sources/UI Layer/Screens/CharacterCreation/CharacterSettingViewModel.swift
@@ -6,12 +6,17 @@
 //
 
 import Foundation
+import RxRelay
 
 final class CharacterSettingViewModel {
+    
+    let characterMeta = BehaviorRelay<CharacterMeta>(value: .default)
     
     init(characterItemRepository: CharacterItemRepository) {
         self.characterItemRepository = characterItemRepository
     }
     
+    
     private let characterItemRepository: CharacterItemRepository
+    
 }
