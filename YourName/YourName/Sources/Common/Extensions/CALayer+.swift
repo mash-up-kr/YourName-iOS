7788fd82dffa7541b2ca1c9bfc7e20c600cc05b5
SeoYoung <bgd216@naver.com>
2021-10-04 20:21:37 +0900

    💄 tabBar height , shadow 적용

--- YourName/YourName/Sources/Common/Extensions/CALayer+.swift
+++ YourName/YourName/Sources/Common/Extensions/CALayer+.swift
@@ -5,14 +5,17 @@
 //  Created by 송서영 on 2021/10/02.
 //
 
-import QuartzCore
-//
-//extension CAShapeLayer {
-//    
-//    func dashBorder() {
-//        let path = CGMutablePath()
-//        let points = [CGPoint.zero, CGPoint(x: , y:)]
-//        path.addLine(between: [CGPoint(x: 0, y: 0),
-//                                CGPoint(x: self.frame.width, y: 0)])
-//    }
-//}
+import UIKit
+
+extension CALayer {
+    func applyShadow(color: CGColor,
+                     alpha: Float,
+                     x: CGFloat,
+                     y: CGFloat,
+                     blur: CGFloat) {
+        self.shadowColor = color
+        self.shadowOpacity = alpha
+        self.shadowOffset = CGSize(width: x, height: y)
+        self.shadowRadius = blur / 2
+    }
+}
