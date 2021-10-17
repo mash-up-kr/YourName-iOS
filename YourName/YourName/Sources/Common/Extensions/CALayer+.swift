4241dc37e98615cd322d85471204c1bd2daf0ddb
Seoyoung Song <42825223+SongSeoYoung@users.noreply.github.com>
2021-10-18 06:44:31 +0900

    [Fix] tabBar UI (#36)
    * 💄 tabBar height , shadow 적용
    
    * ⚡️ CALayer+ apply shadow default값 추가, height조정 코드 제거

--- YourName/YourName/Sources/Common/Extensions/CALayer+.swift
+++ YourName/YourName/Sources/Common/Extensions/CALayer+.swift
@@ -5,14 +5,17 @@
 //  Created by 송서영 on 2021/10/02.
 //
 
-import QuartzCore
-//
-//extension CAShapeLayer {
-//    
-//    func dashBorder() {
-//        let path = CGMutablePath()
-//        let points = [CGPoint.zero, CGPoint(x: , y:)]
-//        path.addLine(between: [CGPoint(x: 0, y: 0),
-//                                CGPoint(x: self.frame.width, y: 0)])
-//    }
-//}
+import UIKit
+
+extension CALayer {
+    func applyShadow(color: CGColor = UIColor.black.cgColor,
+                     alpha: Float = 0.25,
+                     x: CGFloat = 0,
+                     y: CGFloat = 0,
+                     blur: CGFloat = 5) {
+        self.shadowColor = color
+        self.shadowOpacity = alpha
+        self.shadowOffset = CGSize(width: x, height: y)
+        self.shadowRadius = blur / 2
+    }
+}
