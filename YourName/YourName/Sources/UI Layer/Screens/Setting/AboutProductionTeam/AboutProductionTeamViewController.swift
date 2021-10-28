24b5059313ae74d5e74c1f27ac405adec97e7aa0
SeoYoung <bgd216@naver.com>
2021-10-28 23:28:46 +0900

    ⚡️ navigation 연결

--- YourName/YourName/Sources/UI Layer/Screens/Setting/AboutProductionTeam/AboutProductionTeamViewController.swift
+++ YourName/YourName/Sources/UI Layer/Screens/Setting/AboutProductionTeam/AboutProductionTeamViewController.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
+import RxSwift
 
 final class AboutProductionTeamViewController: ViewController, Storyboarded {
 
     @IBOutlet unowned var naviBackButton: UIButton!
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabBarController?.tabBar.isHidden = true
+        bind()
     }
-    
 }
 
 extension AboutProductionTeamViewController {
     func bind() {
+        naviBackButton.rx.throttleTap
+            .bind(onNext: { [weak self] in
+                self?.closeOverlayViewControllers()
+            })
+            .disposed(by: disposeBag)
     }
 }
