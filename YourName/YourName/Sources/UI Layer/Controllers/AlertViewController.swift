3d5600c93b1edf590570d4be6a2e5f7082c58d8a
SeoYoung <bgd216@naver.com>
2021-11-07 21:31:23 +0900

    ✨ alert component 제작

---
+++ YourName/YourName/Sources/UI Layer/Controllers/AlertViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AlertViewController.swift
+//  MEETU
+//
+//  Created by 송서영 on 2021/11/07.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct AlertItem {
+    struct AlertAction {
+        let title: String
+        let action: () -> Void
+    }
+    let title: String
+    let message: String
+    let image: UIImage
+    let emphasisAction: AlertAction
+    let defaultAction: AlertAction
+}
+
+final class AlertViewController: ViewController, Storyboarded {
+
+    @IBOutlet private unowned var titleLabel: UILabel!
+    @IBOutlet private unowned var messageLabel: UILabel!
+    @IBOutlet private unowned var meetUImageView: UIImageView!
+    @IBOutlet private unowned var emphasisButton: UIButton!
+    @IBOutlet private unowned var defaultButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    func show(item: AlertItem) {
+        self.titleLabel.text = item.title
+        self.messageLabel.text = item.message
+        self.meetUImageView.image = item.image
+        self.emphasisButton.setTitle(item.emphasisAction.title, for: .normal)
+        self.defaultButton.setTitle(item.defaultAction.title, for: .normal)
+        
+        self.emphasisButton.rx.throttleTap
+            .bind(onNext: { item.emphasisAction.action() })
+            .disposed(by: disposeBag)
+        
+        self.defaultButton.rx.throttleTap
+            .bind(onNext: { item.defaultAction.action() })
+            .disposed(by: disposeBag)
+        
+        self.modalPresentationStyle = .fullScreen
+        self.modalTransitionStyle = .crossDissolve
+    }
+}
