099f228d7d6adca23aba26f8a8f3fa5da6d9df63
Booung <dongyoung.lee@naverz-corp.com>
2021-11-24 23:48:24 +0900

    강점 Repository 구현

--- YourName/YourName/Sources/Domain Layer/StrongPoint/StrongPointRepository.swift
+++ YourName/YourName/Sources/Domain Layer/StrongPoint/StrongPointRepository.swift
@@ -12,3 +12,26 @@ protocol StrongPointRepository {
     func fetchAll() -> Observable<[StrongPoint]>
 }
 
+
+final class StrongPointRepositoryImpl: StrongPointRepository {
+    
+    init(network: NetworkServing = Enviorment.current.network) {
+        self.network = network
+    }
+    
+    func fetchAll() -> Observable<[StrongPoint]> {
+        return network.request(BehaviorsAPI())
+            .compactMap { [weak self] behaviors in
+                guard let self = self else { return nil }
+                return behaviors.list.compactMap(self.translate(fromTMI:))
+            }
+    }
+    
+    private func translate(fromTMI tmi: Entity.TMI) -> StrongPoint? {
+        guard let content = tmi.value else { return nil }
+        return StrongPoint(content: content)
+    }
+    
+    private let network: NetworkServing
+    
+}
